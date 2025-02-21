d7a425fc1d7ae93b4593ea8ef33ec766ed4d143b
Anton <anton_tarakin@mail.ru>
2025-02-21 19:35:54 +0300

    four

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
-import readlineSync from 'readline-sync'
-export const greetings = () => {
-	console.log('Welcome to the Brain Games!');
-	const nameRequest = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${nameRequest}!`);
-}
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const nameRequest = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameRequest}!`);
+};
+export default greetings;
