57fb1b6dae07e45ef286e04297b15443fa4fb1e0
Anton <anton_tarakin@mail.ru>
2025-02-25 22:11:05 +0300

    seven

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,53 +1,48 @@
 import readlineSync from 'readline-sync';
 
-const parityCheck = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const nameRequest = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${nameRequest}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-  const randomNumberOne = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + Math.ceil(1)));
-  const randomNumberTwo = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + Math.ceil(1)));
-  const randomNumberThree = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + Math.ceil(1)));
-  console.log(`Question: ${randomNumberOne}`);
-  const numberUser = readlineSync.question('Your answer: ');
-  if ((randomNumberOne % 2 === 0 & numberUser === 'yes') || (randomNumberOne % 2 !== 0 & numberUser === 'no')) {
-    console.log('Correct!');
-    console.log(`Question: ${randomNumberTwo}`);
-    const numberUser = readlineSync.question('Your answer: ');
-    if ((randomNumberTwo % 2 === 0 & numberUser === 'yes') || (randomNumberTwo % 2 !== 0 & numberUser === 'no')) {
-      console.log('Correct!');
-      console.log(`Question: ${randomNumberThree}`);
-      const numberUser = readlineSync.question('Your answer: ');
-      if ((randomNumberThree % 2 === 0 & numberUser === 'yes') || (randomNumberThree % 2 !== 0 & numberUser === 'no')) {
-        console.log('Correct!');
-        console.log(`Congratulations, ${nameRequest}!`);
-      } else if (randomNumberThree % 2 === 0 & numberUser === 'no') {
-        console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
-        console.log(`Let's try again, ${nameRequest}!`);
-      } else if (randomNumberThree % 2 !== 0 & numberUser === 'yes') {
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
-        console.log(`Let's try again, ${nameRequest}!`);
-      } else {
-        console.log('error');
-      }
-    } else if (randomNumberTwo % 2 === 0 & numberUser === 'no') {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
-      console.log(`Let's try again, ${nameRequest}!`);
-    } else if (randomNumberTwo % 2 !== 0 & numberUser === 'yes') {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
-      console.log(`Let's try again, ${nameRequest}!`);
-    } else {
-      console.log('error');
-    }
-  } else if (randomNumberOne % 2 === 0 & numberUser === 'no') {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
-    console.log(`Let's try again, ${nameRequest}!`);
-  } else if (randomNumberOne % 2 !== 0 & numberUser === 'yes') {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
-    console.log(`Let's try again, ${nameRequest}!`);
-  } else {
-    console.log(`${numberUser} is wrong answer ;(. Correct answer was 'no'`);
-    console.log(`Let's try again, ${nameRequest}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+}
+
+const genInt = () => { 
+  return Math.floor(Math.random() * 100);
+}
+
+const askQuestion = (name) => {
+  const x = genInt();
+  console.log(`Question: ${x}`);
+  let correctAnswer;
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (x % 2 === 0) {
+    correctAnswer = 'yes';
   }
-};
+  else {
+    correctAnswer = 'no';
+  }
+  if (correctAnswer === userAnswer) {
+    console.log("Correct!");
+    return true;
+  } 
+  else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  }
+}
+
+const parityCheck = () => {
+  const name = greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  const numberOfQuestions = 3;
+  for (let i = 0; i < numberOfQuestions; i++) {
+    if (!askQuestion(name)) {
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+}
+
 parityCheck();
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,12 +1,71 @@
 import readlineSync from 'readline-sync';
 
-const parityCheck = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const nameRequest = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${nameRequest}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-  const randomNumberOne = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + Math.ceil(1)));
-  const randomNumberTwo = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + Math.ceil(1)));
-  const randomNumberThree = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + Math.ceil(1)));
-  console.log(`Question: ${randomNumberOne}`);
-  const numberUser = readlineSync.question('Your answer: ');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+}
+
+const genInt = () => { 
+  return Math.floor(Math.random() * 50);
+}
+
+const askQuestion = (name) => {
+  const x = genInt();
+  const y = genInt();
+  console.log(`Question: ${x} ${y}`);
+  const userAnswer = Number(readlineSync.question('Your answer: '));
+  let max;
+  let min;
+  let intermediate;
+  let expected;
+  let a;
+  if (x >= y) {
+    max = x;
+    min = y;
+  }
+  else {
+    max = y;
+    min = x;
+  }
+  for (let i = min; i <= max; i++) {
+    if (max - min === 0) {
+      expected = max;
+      return expected;
+    }
+    else {
+      intermediate = max - min;
+      max = min;
+      min = intermediate;
+      if (max < min) {
+        a = min;
+        min = max;
+        max = a;
+      }
+    }
+  }
+  if (userAnswer === expected) {
+    console.log("Correct!");
+    return true;
+  } 
+  else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expected}'`);
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  }
+}
+
+const parityCheck = () => {
+  const name = greeting();
+  console.log('Find the greatest common divisor of given numbers');
+  const numberOfQuestions = 3;
+  for (let i = 0; i < numberOfQuestions; i++) {
+    if (!askQuestion(name)) {
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+}
+
+parityCheck(); 
