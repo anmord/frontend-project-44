445ac579a151ff386a484cb8b0bb123c45453810
Anton <anton_tarakin@mail.ru>
2025-03-02 20:27:58 +0300

    игра Арифметическая прогрессия

---
+++ src/games/brain-progression.js
@@ -0,0 +1,22 @@
+import { genInt } from '../utils.js';
+import launch from './selectionofgames.js';
+
+const rule = 'What number is missing in the progression?';
+
+const genQuestion = () => {
+  const sequenceLength = genInt(5, 10);
+  const sequenceStep = genInt(2, 4);
+  const sequenceOfNumbers = [];
+  const firstNumber = genInt(0, 20);
+  const unknownNumber = genInt(0, sequenceLength - 1);
+  sequenceOfNumbers.push(firstNumber);
+  for (let i = 0; i < (sequenceLength - 1); i += 1) {
+    sequenceOfNumbers.push(Number(sequenceOfNumbers[i]) + sequenceStep);
+  }
+  const answer = sequenceOfNumbers[unknownNumber];
+  sequenceOfNumbers[unknownNumber] = '..';
+  const question = `Question: ${sequenceOfNumbers.join(' ')}`;
+  return [question, answer];
+};
+
+export default () => launch(rule, genQuestion);
